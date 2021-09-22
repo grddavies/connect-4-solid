c8b4e22cc8e7016dd92f1a4098719ae2d02b4668
gdavies <grd.davies@gmail.com>
2021-09-22 02:53:01 +0100

    Colour counter by square value

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,19 +1,25 @@
 import { Component, For } from "solid-js";
-import Square from "./Square";
+import Square, { SquareVal } from "./Square";
 
 const Grid: Component = () => {
   const nrows = 6,
     ncols = 7,
     rows = [...Array(nrows).keys()],
-    cols = [...Array(ncols).keys()];
+    cols = [...Array(ncols).keys()],
+    vals: SquareVal[] = [null, -1, 1];
   return (
     <div>
       <For each={rows}>
         {(i) => (
-          <div className="board-row">
+          <div className="grid-row">
             <For each={cols}>
               {(j) => {
-                return <Square />;
+                  const foo = Math.floor(Math.random() * vals.length)
+                  return (
+                  <Square
+                    value={vals[foo]}
+                  ></Square>
+                );
               }}
             </For>
           </div>
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,13 +1,23 @@
-import { Component } from "solid-js";
+import { Component, ComponentProps } from "solid-js";
 
 // Square value can be null, -1, 1 where -1/1 are colours
 // Square inner changes colour with this value
 
-const Square: Component = () => (
-<div className="square-outer">
-    <div className="square-inner">
-    </div>
-</div>
-);
+export type SquareVal = 1 | -1 | null;
 
-export default Square
+export interface SquareProps extends ComponentProps<"div"> {
+  value: SquareVal;
+}
+
+const Square: Component<SquareProps> = (props: SquareProps) => {
+  let v = props.value;
+  return (
+    <div className="outer">
+      <div
+        className={"inner" + (v ? " inner-" + (v === 1 ? "p1" : "p2") : "")}
+      ></div>
+    </div>
+  );
+};
+
+export default Square;
