65f0037100431eed6c2fc40d0afc2a5b8a4bbdac
Gethin Davies <grd.davies@gmail.com>
2021-12-21 12:12:08 +0000

    Add grid border elements

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -9,28 +9,54 @@ interface GridProps extends ComponentProps<"div"> {
   nrow: number;
 }
 
+const borderClass = (i, j, rows, cols) => {
+  let top = i === rows.at(0) ? "border-h" : "",
+    bottom = i === rows.at(-1) ? "border-h" : "",
+    left = j === cols.at(0) ? "border-v" : "",
+    right = j === cols.at(-1) ? "border-v" : "",
+    corner = 
+      (top && left
+        ? "border-corner border-tl"
+        : top && right
+        ? "border-corner border-tr"
+        : bottom && left
+        ? "border-corner border-bl"
+        : bottom && right
+        ? "border-corner border-br"
+        : "");
+  return [top, bottom, left, right, corner].filter((x) => x !== "").join(" ")
+};
+
 const Grid: Component<GridProps> = (props: GridProps) => {
   const nrows = props.nrow,
     ncols = props.ncol,
-    rows = [...Array(nrows).keys()],
-    cols = [...Array(ncols).keys()];
+    rows = [...Array(nrows + 2).keys()], // +2 for borders
+    cols = [...Array(ncols + 2).keys()]; // +2 for borders
   return (
     <div>
       <For each={rows}>
-        {(i) => (
-          <div className="grid-row">
-            <For each={cols}>
-              {(j) => {
-                return (
-                  <Square
-                    value={props.gridvals[i + nrows * j]}
-                    onClick={() => props.clickhandler(i + nrows * j)}
-                  />
-                );
-              }}
-            </For>
-          </div>
-        )}
+        {(i) => {
+          return (
+            <div className="grid-row">
+              <For each={cols}>
+                {(j) => {
+                  let thisborderClass = borderClass(i, j, rows, cols),
+                    sqid = i - 1 + nrows * (j - 1);
+                  if (thisborderClass) {
+                    return <div class={thisborderClass}></div>;
+                  } else {
+                    return (
+                      <Square
+                        value={props.gridvals[sqid]}
+                        onClick={() => props.clickhandler(sqid)}
+                      />
+                    );
+                  }
+                }}
+              </For>
+            </div>
+          );
+        }}
       </For>
     </div>
   );
